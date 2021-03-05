f13b5c1db4c8468efba849d60ca3fb53139620a8
werdnaknom <andrew.m.monk@gmail.com>
2021-03-05 00:25:03 -0800

    Adding classes, cleaning up writers and adding tests.

--- run_me.py
+++ run_me.py
@@ -1,11 +1,5 @@
-#from document_writer import DocumentWriter
 from app.document_writer import DocumentWriter
 
 if __name__ == '__main__':
-    '''
-    doc_writer = DocumentWriter(configuration_file="config.ini")
-
-    doc_writer.run()
-    '''
     doc_writer = DocumentWriter(configuration_file="config.ini")
     doc_writer.run()
